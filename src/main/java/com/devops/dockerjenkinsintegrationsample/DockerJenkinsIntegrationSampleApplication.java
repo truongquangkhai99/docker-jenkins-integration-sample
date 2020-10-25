71f7eef5fad4e04dc6e33f384094ad0aefce7dd1
MKH005 <jienieVN-dev@jienielab.co.jp>
2020-10-25 10:28:40 +0700

    first commit

---
+++ src/main/java/com/devops/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.devops.dockerjenkinsintegrationsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.dockerjenkinsintegrationsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
